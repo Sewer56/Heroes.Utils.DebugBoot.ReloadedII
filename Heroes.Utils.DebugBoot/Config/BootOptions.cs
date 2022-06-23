4316ee3be10b4fdb1bab81afee818d91d512c4c8
Sewer 56 <seweratwork@gmail.com>
2022-06-23 15:52:14 +0100

    Added: Enhanced Config Menu (Reloaded 1.19.0)

--- Heroes.Utils.DebugBoot/Config/BootOptions.cs
+++ Heroes.Utils.DebugBoot/Config/BootOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Heroes.Utils.DebugBoot.Heroes;
+using System.ComponentModel;
 
 namespace Heroes.Utils.DebugBoot.Config;
 
@@ -13,9 +14,24 @@ public struct BootOptions
         TeamP4 = Team.Null
     };
 
+    [DisplayName("Stage")]
+    [Description($"The stage to boot into when BootMode is set to InGame.")]
+    [DefaultValue(Stage.SeasideHill)]
     public Stage Stage { get; set; }
+
+    [DisplayName("Player 1 Team")]
+    [DefaultValue(Team.Sonic)]
     public Team TeamP1 { get; set; }
+
+    [DisplayName("Player 2 Team")]
+    [DefaultValue(Team.Null)]
     public Team TeamP2 { get; set; }
+
+    [DisplayName("Player 3 Team")]
+    [DefaultValue(Team.Null)]
     public Team TeamP3 { get; set; }
+
+    [DisplayName("Player 4 Team")]
+    [DefaultValue(Team.Null)]
     public Team TeamP4 { get; set; }
 }
--- Heroes.Utils.DebugBoot/Config/Config.cs
+++ Heroes.Utils.DebugBoot/Config/Config.cs
@@ -6,12 +6,23 @@ namespace Heroes.Utils.DebugBoot.Config;
 
 public class Config : Configurable<Config>
 {
+    const string SystemModeDescription = "PalSelect: PS2 Leftover. Allows to select 50/60Hz modes.\n" +
+                                         "MainMenu: Boots to Main Menu\n" +
+                                         "InGame: Boots inside a Stage\n" +
+                                         "EasyMenu: Boots into Debug Menu\n" +
+                                         "Credits: Boots into Credits (buggy)\n" +
+                                         "EasyMenuMovie: Boots into Movie/FMV Selection";
+
     [DisplayName("Main Menu Mode")]
-    [Description("Decides which mode the game should enter when entering the main menu.")]
+    [Description("Decides which mode the game should enter when entering the main menu.\n" +
+                 $"{SystemModeDescription}")]
+    [DefaultValue(SystemMode.EasyMenu)]
     public SystemMode MainMenuMode { get; set; } = SystemMode.EasyMenu;
 
     [DisplayName("Boot Mode")]
-    [Description("Decides into which mode the game should boot into on launch.")]
+    [Description("Decides into which mode the game should boot into on launch.\n" +
+                 $"{SystemModeDescription}")]
+    [DefaultValue(SystemMode.EasyMenu)]
     public SystemMode BootMode     { get; set; } = SystemMode.EasyMenu;
 
     [DisplayName("Boot Options")]
